41892bbba52313414b0da8ffeca66ab866be14de
Wojcirej <wojciech.bal@epoczta.pl>
2019-10-25 21:39:20 +0200

    Logic fetching presigned POST link from the backend.

--- frontend/app/static/js/app.js
+++ frontend/app/static/js/app.js
@@ -1,1 +1,20 @@
-console.log("Hello, I am included!");
+const getPresignedPostLink = async() => {
+  const credentials = await fetchCredentials();
+  storePresignedPostLink(credentials);
+};
+
+const fetchCredentials = async() => {
+  const response = await fetch('http://localhost:3000/upload_credentials.json');
+  const myJson = await response.json();
+  return myJson;
+};
+
+const storePresignedPostLink = (link) => {
+  sessionStorage.setItem('link', JSON.stringify(link));
+};
+
+const getPresignedPostLinkFromStorage = () => {
+  return JSON.parse(sessionStorage.getItem('link'));
+}
+
+getPresignedPostLink();
